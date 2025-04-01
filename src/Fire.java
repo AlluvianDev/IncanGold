457a8827ff69b024eb64ca9e2830fa749b34055f
AlluvianDev <ar.zeytin@hotmail.com>
2025-04-01 16:06:43 +0300

    1 nisan commit 3

--- src/Fire.java
+++ src/Fire.java
@@ -1,15 +1,2 @@
 public class Fire extends HazardCard {
-    public Fire(String name) {
-        super(name);
-    }
-    
-    @Override
-    public void Attack() {
-        System.out.println("You are attacked by flames!");
-    }
-    
-    @Override
-    public String toString() {
-        return "Fire (Hazard)";
-    }
 }
